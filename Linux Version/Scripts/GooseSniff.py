debae7a7ac70f1dd26e1fe383e9fe28bd742a54d
whotmhk <157096339+whotmhk@users.noreply.github.com>
2024-07-25 20:18:00 +0800

    Update GooseSniff.py

--- Linux Version/Scripts/GooseSniff.py
+++ Linux Version/Scripts/GooseSniff.py
@@ -1,5 +1,6 @@
 #############################################################################
 # This is for MSSD Project Student ID 1007386
+# Titled : Cyber Range of Critical Infrastructure for Cybersecurity Experiment
 # Reference Goosestalker
 ############################################################################
 import datetime
